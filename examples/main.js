73ca83c92daa68b903c8d272c8ed140792d4dc67
adcentury <adcentury100@gmail.com>
2016-01-13 15:07:43 +0800

    init

---
+++ examples/main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './containers/app';
+
+import './example.less';
+
+render(
+  <App />,
+  document.getElementById('app')
+);
