0f6404e6147bf67323ab11785bdcabd5b0188e3f
Adcent <adcentury100@gmail.com>
2025-05-01 06:40:08 +0800

    hotfix: fix build bug

--- vite.config.examples.ts
+++ vite.config.examples.ts
@@ -1,15 +1,17 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      'react-mobile-picker': resolve(__dirname, './lib'),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "react-mobile-picker": resolve(__dirname, isProd ? "." : "./lib"),
+      },
     },
-  },
-})
+  };
+});
